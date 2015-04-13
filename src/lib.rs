3e8ed64341b99c41f7440bdb169484c72177c7d0
Ben Batha <bhbatha@gmail.com>
2015-04-12 22:17:16 -0700

    feat(pool) Add a basic thread pool
    Not sure how to test this correctly. The current test is watch
    the output to see if it looks like there's some batching going on
    ... gross.

---
+++ src/lib.rs
@@ -0,0 +1,41 @@
+#![feature(std_misc)]
+use std::sync::Semaphore;
+use std::thread;
+
+pub struct ThreadPool {
+    sem: std::sync::Semaphore,
+}
+
+impl ThreadPool {
+    pub fn new(size: isize) -> ThreadPool {
+       ThreadPool { sem: std::sync::Semaphore::new(size) }
+    }
+
+    pub fn spawn<'a, T, F>(&'a self, func: F) -> std::thread::JoinGuard<'a, T>
+        where T: Send + 'a, F: FnOnce() -> T, F: Send + 'a
+    {
+       thread::scoped(move || {
+           let guard = self.sem.access();
+           func()
+       })
+    }
+}
+
+#[test]
+fn it_works() {
+    let pool = ThreadPool::new(2);
+    pool.spawn(move || {
+        std::thread::sleep_ms(1000);
+        println!("Done!");
+    });
+
+    pool.spawn(move || {
+        std::thread::sleep_ms(1000);
+        println!("Done!");
+    });
+
+    pool.spawn(move || {
+        std::thread::sleep_ms(1000);
+        println!("Done!");
+    })
+}
